0d719f75f063b90695c1f9f2197b78047fc11217
Eli <136011323+eliaselliotson@users.noreply.github.com>
2026-07-06 23:44:22 +0000

    Revamp App component layout and styling for improved visual appeal

--- src/src/App.jsx
+++ src/src/App.jsx
@@ -4,14 +4,14 @@ import { experience, projects, profiles } from "./constants.jsx";
 function App() {
   return (
     <>
-      <div className="w-full flex flex-col gap-20 my-15 md:my-20 lg:my-30 px-[10%]">
-        <div className="w-full flex flex-col items-center gap-3 md:gap-4 lg:gap-5 text-center my-10 md:my-20 lg:my-40 2xl:my-60">
-          <h1 className="font-[Geom] font-black text-5xl md:text-7xl lg:text-8xl text-blue-600">Elias Elliotson</h1>
-          <p className="font-[Geom] text-base lg:text-xl font-medium text-gray-700 text-center">
-            I'm a 17-year-old web developer with over 3 years of professional experience.
-          </p>
-        </div>
+      <div className="w-full h-[75vh] max-h-[75vw] flex flex-col items-center justify-center gap-3 md:gap-4 lg:gap-5 text-center px-[10%]" style={{ background: "linear-gradient(#ffffffcc, #ffffff00), radial-gradient(#ffffff, #eeeeee, #cccccc)" }}>
+        <h1 className="font-[Geom] font-black text-5xl md:text-7xl lg:text-8xl text-fuchsia-500 text-transparent bg-clip-text" style={{ backgroundImage: "linear-gradient(135deg, #e12afb, #155dfc)", WebkitBackgroundClip: "text" }}>Elias Elliotson</h1>
+        <p className="font-[Geom] text-base lg:text-xl font-medium text-gray-700 text-center">
+          I'm a 17-year-old web developer with over 3 years of professional experience.
+        </p>
+      </div>
 
+      <div className="w-full flex flex-col gap-15 md:gap-20 my-15 md:my-20 px-[10%]">
         <div className="w-full flex flex-col items-center gap-3 md:gap-5 lg:gap-8">
           <h1 className="font-[Geom] font-black text-2xl md:text-3xl lg:text-4xl text-center">Experience</h1>
           <div className="flex flex-col justify-center gap-2">
